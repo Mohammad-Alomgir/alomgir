75de722bb7804232e0af89e44931cf0fbee925cf
Mohammad-Alomgir <expectan-coder@gmail.com>
2024-04-25 16:26:02 +0600

    second commit

--- main.js
+++ main.js
@@ -4,4 +4,11 @@ const type = new Typed('.auto-type',{
     typeSpeed: 200,
     backSpeed: 150,
     loop: true
+});
+
+// toggle able navbar
+const btn = document.getElementById('menu-icon');
+const navList = document.querySelector('.nav-list');
+btn.addEventListener('click',() => {
+  navList.classList.toggle('active');
 })
