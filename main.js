53ac9a4de29c86e915c940b6b108c881acc1fe69
Mohammad-Alomgir <expectan-coder@gmail.com>
2024-04-25 22:13:16 +0600

    third commit

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
 // Auto-typing-text
-const type = new Typed('.auto-type',{
-    strings: ['Reality','Innovate ','Create ','Achieve '],
-    typeSpeed: 200,
-    backSpeed: 150,
-    loop: true
+const type = new Typed(".auto-type", {
+  strings: ["Reality", "Innovate ", "Create ", "Achieve "],
+  typeSpeed: 200,
+  backSpeed: 150,
+  loop: true,
 });
 
 // toggle able navbar
-const btn = document.getElementById('menu-icon');
-const navList = document.querySelector('.nav-list');
-btn.addEventListener('click',() => {
-  navList.classList.toggle('active');
-})
+const menubar = document.getElementById("menu-icon");
+console.log(menubar);
+const closebar = document.getElementById("menu-close-icon");
+const navbar = document.querySelector(".menu");
+function toggleMenu() {
+  // menubar.classList.toggle("active");
+  // closebar.classList.toggle("active");
+  navbar.classList.toggle("active");
+}
+
+menubar.addEventListener("click", toggleMenu);
+closebar.addEventListener('click',toggleMenu);
+// toggl class when scroll
+const hello = window.pageYOffset;
+console.log(hello);
+const initialInnerHeight = window.innerHeight;
+window.onscroll = () => {
+  if(initialInnerHeight > window.innerHeight){
+    navbar.classList.toggle('active')
+  }
+}
